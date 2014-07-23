2d4e6ba7c7b59e78214516c9d5b98fc1663997b2
andrefau <andrefau@student.hist.no>
2014-07-23 16:07:13 +0200

    Work on level1 class

--- src/Level1.java
+++ src/Level1.java
@@ -16,7 +16,7 @@ public class Level1 {
                 "'Use <item> with <object>'   - Uses an item from the world, or your inventory, with an object in the world.\n");
     }
 
-    public void look () {
+    public void look() {
         System.out.println("You are standing in a small square room, with a stone floor, wooden walls, a steel door and no windows.\n" +
                 "A dim light comes from beneath the door, enough to slightly illuminate the small room. In the middle of the room " +
                 "there's a table with a note on it.\n");
@@ -27,8 +27,12 @@ public class Level1 {
             System.out.println("It is a cold and hard stone floor.\n");
         } else if (object.equalsIgnoreCase("walls") || object.equalsIgnoreCase("wall")) {
             System.out.println("The walls are made of old, splintery wood.\n");
+        } else if (object.equalsIgnoreCase("door")) {
+            System.out.println("You see a large steel door. A faint light emanates through the crack beneath it.\n");
         } else if (object.equalsIgnoreCase("table")) {
             System.out.println("You see a small wooden table with four legs. There's a note on it.\n");
+        } else if (object.equalsIgnoreCase("note")) {
+            System.out.println("");
         } else {
             System.out.println("You want to look at what?.\n");
         }
